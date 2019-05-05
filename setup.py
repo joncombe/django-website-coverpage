427db9179843aeae795d0054e349695680f7bbde
Jon Combe <github@joncombe.net>
2019-05-05 03:52:54 +0000

    Improved template

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-website-coverpage',
-    version='0.0.4',
+    version='0.0.5',
     author=u'Jon Combe',
     author_email='pypi@joncombe.net',
     packages=find_packages(),
